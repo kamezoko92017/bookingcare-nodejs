a84bb9d34744be518fa1d769ac9d08fdba88c277
albertbrasi <albertbrasi001@gmail.com>
2022-06-28 11:48:36 +0700

    Video#21 Kết Nối Tới MySQL Database với Sequelize - Render Dữ Liệu Ra View

--- src/server.js
+++ src/server.js
@@ -2,7 +2,10 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine"
 import initWebRoutes from "./route/web"
+import connectDB from "./config/connectDB"
+
 require("dotenv").config();
+
 
 let app = express()
 
@@ -12,6 +15,8 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 viewEngine(app)
 initWebRoutes(app)
+
+connectDB()
 
 let port = process.env.PORT || 6969
 
