806c7b750572bc0b93962f2a76323b02641beb6b
albertbrasi <albertbrasi001@gmail.com>
2022-06-28 21:41:26 +0700

    Video#23+Video#24 Tạo Tất Cả Models, Migrations và Seeders cho project

--- src/seeders/seeder-demo-user.js
+++ src/seeders/seeder-demo-user.js
@@ -2,6 +2,18 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [{
+      email: 'admin@gmail.com',
+      password: '12345',
+      firstName: 'Nguyen',
+      lastName: 'Hai',
+      address: 'USA',
+      gender: 1,
+      typeRole: 'ROLE',
+      keyRole: 'R1',
+      createAt: new Date(),
+      updateAt: new Date()
+    }])
     /**
      * Add seed commands here.
      *
